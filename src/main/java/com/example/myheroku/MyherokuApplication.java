51cd37e206eeebb8953e74db19d166c89f508b78
mohanbera <immohanbera777@gmail.com>
2021-03-31 23:37:39 +0530

    initial commit

---
+++ src/main/java/com/example/myheroku/MyherokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.myheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyherokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyherokuApplication.class, args);
+	}
+
+}
